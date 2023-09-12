b7c6696fac69a37dac95ac35020d28ee2da18b2f
Pauline <pauline_soubrie@hotmail.fr>
2023-09-12 11:42:05 +0200

    add timeline animation + all projects links

---
+++ assets/js/timeline.js
@@ -0,0 +1,41 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".b1", {
+  scrollTrigger: {
+    trigger: ".b1",
+    start: "bottom bottom",
+  },
+  opacity: 1,
+  duration: 2,
+});
+gsap.to(".b2", {
+  scrollTrigger: {
+    trigger: ".b2",
+    start: "50% bottom",
+  },
+  opacity: 1,
+  duration: 2,
+});
+gsap.to(".b3", {
+  scrollTrigger: {
+    trigger: ".b3",
+    start: "50% bottom",
+  },
+  opacity: 1,
+  duration: 2,
+});
+gsap.to(".b4", {
+  scrollTrigger: {
+    trigger: ".b4",
+    start: "50% bottom",
+  },
+  opacity: 1,
+  duration: 2,
+});
+gsap.to(".b5", {
+  scrollTrigger: {
+    trigger: ".b5",
+    start: "50% bottom",
+  },
+  opacity: 1,
+  duration: 2,
+});
