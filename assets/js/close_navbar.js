4a18ca06935e20ceaeef3ebe43341f4a149f29e2
Pauline <pauline_soubrie@hotmail.fr>
2023-09-15 10:04:55 +0200

    fix closes when click on nav link

--- assets/js/close_navbar.js
+++ assets/js/close_navbar.js
@@ -3,6 +3,10 @@ const navElements = document.getElementsByClassName("nav");
 for (let i = 0; i < navElements.length; i++) {
   navElements[i].addEventListener("click", () => {
     const navBar = document.getElementById("nav-small-screens");
+    const burger = document.getElementById("burger");
+    const closeBurger = document.getElementById("close-burger");
     navBar.style.display = "none";
+    burger.style.display = "inline";
+    closeBurger.style.display = "none";
   });
 }
