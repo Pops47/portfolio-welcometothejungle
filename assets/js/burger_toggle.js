f0fe69653fd3ff05770b6e568442dba120d151e8
Pauline <pauline_soubrie@hotmail.fr>
2023-09-15 09:48:24 +0200

    improve burger menu with closing icon

--- assets/js/burger_toggle.js
+++ assets/js/burger_toggle.js
@@ -1,10 +1,20 @@
 const burger = document.getElementById("burger");
+const closeBurger = document.getElementById("close-burger");
 
 burger.addEventListener("click", (e) => {
   e.preventDefault();
   const nav = document.getElementById("nav-small-screens");
 
-  nav.style.display === "none"
-    ? (nav.style.display = "flex")
-    : (nav.style.display = "none");
+  nav.style.display = "flex";
+  burger.style.display = "none";
+  closeBurger.style.display = "inline";
+});
+
+closeBurger.addEventListener("click", (e) => {
+  e.preventDefault();
+  const nav = document.getElementById("nav-small-screens");
+
+  nav.style.display = "none";
+  burger.style.display = "inline";
+  closeBurger.style.display = "none";
 });
